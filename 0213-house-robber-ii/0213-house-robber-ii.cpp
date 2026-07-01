06bce51022312c1606c67f8592ba2efee5506132
KhushieSharma <154341694+KhushieSharma@users.noreply.github.com>
2026-07-01 19:16:10 +0530

    Time: 0 ms (100%), Space: 10.2 MB (87.63%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -0,0 +1,18 @@
+
+class Solution {
+public:
+    long long s(vector<int>&nums,int l,int r){
+        long long prev=0,maxi=0;
+        for(int i=l;i<=r;i++){
+            long long temp=max(maxi,prev+nums[i]);
+            prev=maxi;
+            maxi=temp;
+        }
+        return maxi;
+    }
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1)return nums[0];
+        return max(s(nums,0,n-2),s(nums,1,n-1));
+    }
+};
